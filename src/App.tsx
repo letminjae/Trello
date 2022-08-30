b42ae8720ff72370856e6a1a89c9e75743281f45
letminjae <cmjj0824@naver.com>
2022-08-30 20:16:44 +0900

    ADD: Multi Boards

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,12 @@ import {
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
+import Board from "./Components/Board";
 import DragabbleCard from "./Components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -22,44 +23,30 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
 `;
 
 function App() {
   const [toDos, setTodos] = useRecoilState(toDoState);
   const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
     if(!destination) return;
-    setTodos((oldTodos) => {
-      const copyTodos = [...oldTodos];
-      // 드래그 로직 : source.index 삭제
-      copyTodos.splice(source.index, 1);
-      // 드래그 로직 : draggableId 추가
-      copyTodos.splice(destination?.index, 0, draggableId);
-      return copyTodos;
-    });
+    // setTodos((oldTodos) => {
+    //   const copyTodos = [...oldTodos];
+    //   // 드래그 로직 : source.index 삭제
+    //   copyTodos.splice(source.index, 1);
+    //   // 드래그 로직 : draggableId 추가
+    //   copyTodos.splice(destination?.index, 0, draggableId);
+    //   return copyTodos;
+    // });
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DragabbleCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+        {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
