6645931beffc1726c358fc948624b9ebf754412b
letminjae <cmjj0824@naver.com>
2022-08-31 22:39:56 +0900

    UPDATE: Same Board Movement

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,12 @@
-import {
-  DragDropContext,
-  Droppable,
-  Draggable,
-  DropResult,
-} from "react-beautiful-dnd";
+import { DragDropContext, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import styled from "styled-components";
 import { toDoState } from "./atoms";
 import Board from "./Components/Board";
-import DragabbleCard from "./Components/DragabbleCard";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 680px;
-  width: 100%;
+  width: 100vw;
   margin: 0 auto;
   justify-content: center;
   align-items: center;
@@ -21,30 +14,36 @@ const Wrapper = styled.div`
 `;
 
 const Boards = styled.div`
-  display: grid;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
   width: 100%;
   gap: 10px;
-  grid-template-columns: repeat(3, 1fr);
 `;
 
 function App() {
   const [toDos, setTodos] = useRecoilState(toDoState);
-  const onDragEnd = ({ draggableId, destination, source }: DropResult) => {
-    if(!destination) return;
-    // setTodos((oldTodos) => {
-    //   const copyTodos = [...oldTodos];
-    //   // 드래그 로직 : source.index 삭제
-    //   copyTodos.splice(source.index, 1);
-    //   // 드래그 로직 : draggableId 추가
-    //   copyTodos.splice(destination?.index, 0, draggableId);
-    //   return copyTodos;
-    // });
+  const onDragEnd = (info: DropResult) => {
+    console.log(info)
+    const { destination, draggableId, source } = info;
+    if (destination?.droppableId === source.droppableId) {
+      // 같은 보드안에서 움직인다면
+      setTodos((allBoards) => {
+        const boardCopy = [...allBoards[source.droppableId]];
+        boardCopy.splice(source.index, 1);
+        boardCopy.splice(destination.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: boardCopy,
+        };
+      });
+    }
   };
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-        {Object.keys(toDos).map((boardId) => (
+          {Object.keys(toDos).map((boardId) => (
             <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
           ))}
         </Boards>
